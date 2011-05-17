35bdee6c4dc23d914e2fd3571413cfced2ec58bc
Francisco Souza <francisco@franciscosouza.net>
2011-05-17 00:43:37 -0300

    100% coverage

--- tests/test_home.py
+++ tests/test_home.py
@@ -11,6 +11,18 @@ class TestHome(mocker.MockerTestCase):
     def tearDown(self):
         self.mocker.reset()
 
+    def test_should_display_a_form_with_the_distributions_and_textarea_for_invalidate_paths(self):
+        ConnectionManager = self.mocker.replace('awsmanager.util.ConnectionManager')
+        ConnectionManager.get_cloudfront_connection()
+        self.mocker.result(MockCloudFrontConnection())
+        self.mocker.replay()
+
+        response = self.client.get('/purge')
+        for cname in ('static.domain.com', 'cdn.domain.uk'):
+            assert cname in response.data
+
+        self.mocker.verify()
+
     def test_should_invalidate_paths_on_amazon_cloudfront(self):
         paths = '/path/to/nowhere\n/path/to/anywhere'
         distribution = 'HD8932993S'
@@ -25,3 +37,5 @@ class TestHome(mocker.MockerTestCase):
 
         for path in paths.split('\n'):
             assert ('<li>%s</li>' % path) in response.data
+
+        self.mocker.verify()
