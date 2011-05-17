5de4d2505d77447ec2185a63efd3e3c9b2808328
Francisco Souza <francisco@franciscosouza.net>
2011-05-17 01:37:54 -0300

    Test for the case the user exists, but the password is wrong

--- tests/test_documents.py
+++ tests/test_documents.py
@@ -1,17 +1,24 @@
 import unittest
 from awsmanager.documents import User
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_false
 
 class TestDocuments(unittest.TestCase):
 
-    def tearDown(self):
-        for user in User.query.filter({}):
-            user.remove()
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User()
+        cls.user.username = 'tester'
+        cls.user.password = '123'
+        cls.user.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.remove()
 
     def test_should_be_able_to_authenticate_an_existing_user_with_right_password(self):
-        user = User(username='tester')
-        user.password = '123'
-        user.save()
-
         authenticated, user = User.query.authenticate(username='tester', password='123')
         assert_true(authenticated)
+
+    def test_should_be_able_to_not_authenticate_an_existing_user_with_wrong_password(self):
+        authenticated, user = User.query.authenticate(username='tester', password='wrong123')
+        assert_false(authenticated)
