80acd4850313fff10efbc296919eb00c004a4774
Francisco Souza <francisco@franciscosouza.net>
2011-05-17 00:43:24 -0300

    More on AWS mocks

--- tests/aws_mocks.py
+++ tests/aws_mocks.py
@@ -7,7 +7,19 @@ class MockBatch(object):
     def to_xml(self):
         return '<dumb><xml>Hi!</xml></dumb>'
 
+class MockDistributionSummary(object):
+
+    def __init__(self, id, cname):
+        self.id = id
+        self.cnames = (cname,)
+
 class MockCloudFrontConnection(object):
 
     def create_invalidation_request(self, distribution_id, paths):
-        return MockBatch(paths)
+        return MockBatch(paths=paths)
+
+    def get_all_distributions(self):
+        return [
+            MockDistributionSummary(id='XUUS9933', cname='static.domain.com'),
+            MockDistributionSummary(id='XUUS9934', cname='cdn.domain.uk'),
+        ]
