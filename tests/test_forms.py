fe6edc783661c1e901968733be67976782f20aab
Francisco Souza <francisco@franciscosouza.net>
2011-05-18 01:13:58 -0300

    Validating login

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -1,5 +1,6 @@
 import awsmanager
 import unittest
+from awsmanager.documents import User
 from awsmanager.forms import LoginForm
 from nose.tools import assert_false, assert_true
 
@@ -8,10 +9,15 @@ class TestForms(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         awsmanager.app.config['CSRF_ENABLED'] = False
+        cls.user = User()
+        cls.user.username = 'admin'
+        cls.user.password = '123456'
+        cls.user.save()
 
     @classmethod
     def tearDownClass(cls):
         awsmanager.app.config['CSRF_ENABLED'] = True
+        cls.user.remove()
 
     def test_login_form_should_validate_presence_of_username(self):
         with awsmanager.app.test_request_context():
@@ -30,5 +36,10 @@ class TestForms(unittest.TestCase):
 
     def test_login_form_should_accept_valid_username_and_password(self):
         with awsmanager.app.test_request_context():
-            form = LoginForm(username='user', password='123456')
+            form = LoginForm(username='admin', password='123456')
             assert_true(form.validate())
+
+    def test_login_form_should_raises_ValidationError_on_login_failure(self):
+        with awsmanager.app.test_request_context():
+            form = LoginForm(username='admin', password='123457')
+            assert_false(form.validate())
