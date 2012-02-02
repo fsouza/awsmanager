453e93fcf4883fc752fc2a0397f7071a2c9ce3e8
Francisco Souza <f@souza.cc>
2012-02-02 15:45:10 -0200

    Removed mongodb dependency

--- awsmanager/__init__.py
+++ awsmanager/__init__.py
@@ -1,16 +1,11 @@
 from awsmanager.views import frontend
 from flask import Flask
-from flaskext.mongoalchemy import MongoAlchemy
-
-db = MongoAlchemy()
 
 def get_app(settings_module='awsmanager.settings'):
     app = Flask(__name__)
     app.config.from_object(settings_module)
 
     app.register_module(frontend)
-
-    db.init_app(app)
 
     return app
 
